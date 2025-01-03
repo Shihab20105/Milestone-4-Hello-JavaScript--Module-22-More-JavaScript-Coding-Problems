87c22a6168d5e0459cfd62ac4cba5f9dcb20b260
shihab20105 <shihabur10@gmail.com>
2025-01-03 23:47:47 +0000

    Explored Module Summary and importance of problem solving

---
+++ 23-10 Module Summary and importance o/summary.js
@@ -0,0 +1,4 @@
+/*                          23-10 Module Summary and importance of problem solving */
+
+
+// Discussion about loop , function , array, if-else, return, typeof, multiply, array includes, slice, object
